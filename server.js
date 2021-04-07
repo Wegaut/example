33f198aa3bd16bd93b614994ea6128d9e615b0dd
Wegaut <jonas@wegaut.com>
2021-04-07 17:15:33 -0500

    port update

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 app.get('/', (req,res)=>res.send("<h1>Updating Port 5000!</h1>"));
 
 app.listen(5000);
-console.log('Sever on port 5000');
+console.log('Sever on port 5000 =)');
