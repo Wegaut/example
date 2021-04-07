4ba0fe85495f27064d3802861b2876612e5b3338
Wegaut <jonas@wegaut.com>
2021-04-07 17:01:44 -0500

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res)=>res.send("Hellow Word con express"));
+app.get('/', (req,res)=>res.send("<h1>Updating Port 5000!</h1>"));
 
-app.listen(3000);
-console.log('Sever on port 3000');
+app.listen(5000);
+console.log('Sever on port 5000');
